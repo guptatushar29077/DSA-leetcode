84aa68d926a4c84727add93b9fb0feaf3e80001d
guptatushar29077 <103180582+guptatushar29077@users.noreply.github.com>
2023-08-18 08:22:59 +0530

    Time: 0 ms (100.00%) | Memory: 6.7 MB (40.36%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.cpp
+++ 118-pascals-triangle/pascals-triangle.cpp
@@ -1,29 +1,29 @@
 class Solution {
 public:
 
-    vector<int> permu(int r)
+vector<int> help(int n)
+{
+    vector<int> ans;
+    ans.push_back(1);
+    int a=1;
+    for(int i=1;i<n;i++)
     {
-        vector<int> temp;
-        temp.push_back(1);
-        long long ans=1;
-        for(int i=1;i<r;i++)
-        {
-            ans*=(r-i);
-            ans/=(i);
-            temp.push_back(ans);
-        }
-        return temp;
-
+        a=a*(n-i);
+        a/=(i);
+        ans.push_back(a);
     }
+    return ans;
+}
     vector<vector<int>> generate(int numRows) {
+
         vector<vector<int>> ans;
         for(int i=1;i<=numRows;i++)
-        {   
-            vector<int> help=permu(i);
-                ans.push_back(help);
+        {
+            vector<int> temp=help(i);
+            ans.push_back(temp);
             
         }
-    return ans;
-
+        return ans;
+        
     }
 };
