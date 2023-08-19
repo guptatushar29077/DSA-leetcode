502d75db408311a5ecafb91af88561d5425e4d0a
guptatushar29077 <103180582+guptatushar29077@users.noreply.github.com>
2023-08-19 16:03:59 +0530

    Time: 2 ms (47.21%) | Memory: 7.1 MB (49.52%) - LeetSync

--- 48-rotate-image/rotate-image.cpp
+++ 48-rotate-image/rotate-image.cpp
@@ -4,16 +4,16 @@ public:
 
 
         int n=matrix.size();
-            for(int i=0;i<n-1;i++)
+        for(int i=0;i<n-1;i++)
+        {
+            for(int j=i+1;j<n;j++)
             {
-                for(int j=i+1;j<n;j++)
-                {
-                    swap(matrix[i][j],matrix[j][i]);
-                }
+                swap(matrix[i][j],matrix[j][i]);
             }
-            for(int i=0;i<n;i++)
-            {
-                reverse(matrix[i].begin(),matrix[i].end());
-            }
+        }   
+        for(int i=0;i<n;i++)
+        {
+            reverse(matrix[i].begin(),matrix[i].end());
+        }
     }
 };
