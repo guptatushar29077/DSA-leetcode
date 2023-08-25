55b900721d38c29ffc6767a089a6b3647499fe85
guptatushar29077 <103180582+guptatushar29077@users.noreply.github.com>
2023-08-25 08:51:00 +0530

    Time: 4 ms (29.63%) | Memory: 6.9 MB (17.08%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.cpp
+++ 118-pascals-triangle/pascals-triangle.cpp
@@ -1,27 +1,25 @@
 class Solution {
 public:
 
-vector<int> help(int n)
+vector<int> get(int n)
 {
     vector<int> ans;
     ans.push_back(1);
-    int a=1;
+    int val=1;
     for(int i=1;i<n;i++)
     {
-        a=a*(n-i);
-        a/=(i);
-        ans.push_back(a);
+        val*=(n-i);
+        val/=i;
+        ans.push_back(val);
     }
     return ans;
 }
     vector<vector<int>> generate(int numRows) {
-
         vector<vector<int>> ans;
         for(int i=1;i<=numRows;i++)
         {
-            vector<int> temp=help(i);
+            vector<int> temp=get(i);
             ans.push_back(temp);
-            
         }
         return ans;
         
