304f1c4b91f5d3d3d9d57f72d9a59b2657e21eb5
guptatushar29077 <103180582+guptatushar29077@users.noreply.github.com>
2023-09-29 09:20:15 +0530

    Time: 12 ms (73.40%) | Memory: 18.3 MB (13.63%) - LeetSync

--- 268-missing-number/missing-number.cpp
+++ 268-missing-number/missing-number.cpp
@@ -1,18 +1,15 @@
 class Solution {
 public:
-    int missingNumber(vector<int>& nums) {
-
-
-
-        int n=nums.size();
+    int missingNumber(vector<int>& arr) {
+        
+        int n=arr.size();
         int ans=0;
         for(int i=0;i<n;i++)
         {
             ans=ans^i;
-            ans=ans^nums[i];
+            ans=ans^arr[i];
         }
-        ans=ans^(n);
+        ans=ans^n;
         return ans;
-        
     }
 };
